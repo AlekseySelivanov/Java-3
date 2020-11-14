40c34a5906a1ee3e5032950da0ac387b56766549
AlekseySelivanov <salex92@mail.ru>
2020-11-14 20:36:19 +0300

    Добавлен файл main

---
+++ src/ru/geekbrains/homework1/Main.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.homework1;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Integer arr[] = {1, 2, 3, 4, 5, 6, 7};
+        swap(arr,1, 4);
+    }
+    public static void swap(Object[] arr, int n1, int n2){
+        System.out.println("Task1: "+Arrays.toString(arr));
+        Object sw = arr[n1];
+        arr[n1]=arr[n2];
+        arr[n2]=sw;
+        System.out.println("The result of the replacement: "+Arrays.toString(arr));
+    }
+}
